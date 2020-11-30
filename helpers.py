c3efae1ce71720afa8b0aa87e101484d522ef498
lustea0201 <alexandre.luster@epfl.ch>
2020-11-30 22:38:37 +0100

    Add helper functions

---
+++ helpers.py
@@ -0,0 +1,35 @@
+import numpy as np 
+
+def bootstrap(x, n_iterations = 10000, gamma = 0.95):
+    """ Return a 'gamma' confidence interval for the mean of x by resampling it n_iterations times.
+    
+    Parameters
+    ----------
+		x: 1D array
+			sequences of values to boostrap on
+		n_iterations: int, optional
+			number of bootstrapping iterations
+		gamma: float in (0, 1), optional
+			confidence level (e.g. 0.95 gives a 95% CI)
+			
+	Returns
+	-------
+		CI_low: float
+			lower quantile of the CI
+		CI_high: float
+			upper quantile of the CI
+    """
+    
+    x = np.array(x)
+    N = len(x)
+    q_high = (1+gamma)/2
+    q_low = (1-gamma)/2
+    
+    sample_mean = np.empty(n_iterations)
+    for i in range(n_iterations):
+        sample = np.random.choice(x, N, replace = True)
+        sample_mean[i] = np.mean(sample)
+    
+    CI_low, CI_high = np.quantile(sample_mean, [q_low, q_high])   
+        
+    return CI_low, CI_high
